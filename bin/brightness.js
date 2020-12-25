e730bebed50fac3555f58e80d68ec0d5af07b510
Neos21 <neos21@gmail.com>
2020-12-26 00:26:20 +0900

    [WIP] Windows

---
+++ bin/brightness.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const brightness = require('../lib/index');
+
+const exec = (methodName, value) => brightness[methodName](value)
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
+
+switch(process.argv[2]) {
+  case 'get':
+    return exec('get');
+  case 'set':
+    return exec('set', process.argv[3]);
+  default:
+    return console.error('Please input a valid sub command');
+}
